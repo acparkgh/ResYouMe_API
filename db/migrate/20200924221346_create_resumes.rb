8247c3676ef33a4ad0f56b8ffa4c1728dd568304
acparkgh <acpark123@gmail.com>
2020-09-25 09:59:11 -0400

    build database

--- db/migrate/20200924221346_create_resumes.rb
+++ db/migrate/20200924221346_create_resumes.rb
@@ -2,6 +2,11 @@ class CreateResumes < ActiveRecord::Migration[6.0]
   def change
     create_table :resumes do |t|
       t.integer :user_id
+      t.text :bio
+      t.text :experience
+      t.text :education
+      t.text :skills
+      t.text :misc
 
       t.timestamps
     end
--- db/migrate/20200924221432_create_job_types.rb
+++ db/migrate/20200924221432_create_job_types.rb
@@ -2,6 +2,7 @@ class CreateJobTypes < ActiveRecord::Migration[6.0]
   def change
     create_table :job_types do |t|
       t.integer :resume_id
+      t.string :type
 
       t.timestamps
     end
