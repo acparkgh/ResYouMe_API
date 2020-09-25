8e6fbd3e15ad6edffae33a43a8b2e53f8265d9e9
Rbascoe <roanbascoe5@gmail.com>
2020-09-25 11:54:53 -0400

    controllers created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    render json: @user
+    render json: @user, include: [:resume => {include:[:job_type]}]
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'job_types/index'
-      get 'job_types/show'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'resumes/index'
-      get 'resumes/show'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'users/index'
-      get 'users/show'
+      resources :users 
+      resources :resumes
+      resources :job_types
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
