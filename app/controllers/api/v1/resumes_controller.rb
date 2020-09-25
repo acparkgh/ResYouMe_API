f459ac611208253563450f8aae5e6ccdb6e7f850
Rbascoe <roanbascoe5@gmail.com>
2020-09-25 18:04:45 -0400

    controllers in prog

--- app/controllers/api/v1/resumes_controller.rb
+++ app/controllers/api/v1/resumes_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ResumesController < ApplicationController
 
   def index
+    byebug
     resumes = Resume.all 
     render json: resumes 
   end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,9 +10,22 @@ class Api::V1::UsersController < ApplicationController
     render json: @user, include: [:resume => {include:[:job_type]}]
   end
 
+  # For SignUp
+
+  def create
+    user = User.new(user_params)
+
+    if user.valid?
+      user.save
+      render json: {user: user}, status: :created
+    else
+      render json: {error: "Failed to create a user"}, status: :not_acceptable
+    end
+  end
+
   private
 
-  def user_parmas
+  def user_params
     params.require(:user).permit(:first_name, :last_name, :address, :phone_number, :email, :password)
   end
 
