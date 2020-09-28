1b3c818c49ca8ad1aa4eb8adaaf7f4c60aa8150c
Rbascoe <roanbascoe5@gmail.com>
2020-09-28 15:56:52 -0400

    added resume create method in controller

--- app/controllers/api/v1/resumes_controller.rb
+++ app/controllers/api/v1/resumes_controller.rb
@@ -6,5 +6,24 @@ class Api::V1::ResumesController < ApplicationController
     render json: resumes 
   end
 
+  # For making Resume
+
+  def create
+    resume = Resume.new(resume_params)
+
+    if resume.valid?
+      resume.save
+      render json: {resume: resume}, status: :created
+    else
+      render json: {error: "Failed to create a resume"}, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def resume_params
+    params.permit(:user_id, :bio, :experience, :education, :skills, :misc)
+  end
+
 
 end
