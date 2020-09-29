4d525331f21458df546a5d07bf1c670a1c7d8867
Rbascoe <roanbascoe5@gmail.com>
2020-09-29 17:52:46 -0400

    added login route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       resources :users 
       resources :resumes
       resources :job_types
+
+      post "/login", to: "auth#create"
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
